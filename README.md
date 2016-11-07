0502c74874a7d4217a06610b059f011a70eb419c
Glenn Basgaard <glenn_basgaard@yahoo.com>
2016-11-07 18:09:17 -0500

    add readme info, add wireframe pics, clean up code, drop attempt at leaderboard with local storage

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # dodger-game
 My first project for General Assembly, a javascript game.
+
+MVP: 
+Host my game on github so that it’s playable. Have a game where the objective is to dodge enemies bouncing around the screen at random for as long as you can. Once you come in contact with an enemy end the game and display the score the time. 
+
+Technologies Utilized: HTML, CSS, JavaScript, and jQuery
+
+User Stories: 
+As a player, I want to have access to this game via the Internet and be able to pass the time.
+
+Unsolved Problems: 
+How to implement session storage to keep a leaderboard.
+How to keep enemies from stalling when they first show up.
+Maintaining collision detection when Bart is stationary.
+
+Pseudocode:
+Change cursor to custom image.
+Control character with cursor movement/key strokes.
+Have enemies fly around screen in random directions.
+Stop game if enemies hit your character.
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,38 @@
 "use strict";
+(function() {
 
-var isBetween = function(a, b, c){
-  return a <= b && b <= c;
-}
+$(document).ready(function() {
 
-var pointIsIn = function(point, box){
-  var A = {x: box.x, y: box.y};
-  var B = {x: box.x + box.width, y: box.y};
-  var C = {x: box.x, y: box.y + box.height};
-  return isBetween(A.x, point.x, B.x) && isBetween(A.y, point.y, C.y);
-};
+// This these next 4 variables relate to my collision detection and came with help from Tims
+  var isBetween = function(a, b, c){
+    return a <= b && b <= c;
+  }
 
-var corners = function(box){
-  return(
-    [
-    {x: box.x, y: box.y},
-    {x: box.x + box.width, y: box.y},
-    {x: box.x + box.width, y: box.y + box.height},
-    {x: box.x, y: box.y + box.height}]
-    );
-};
+  var pointIsIn = function(point, box){
+    var A = {x: box.x, y: box.y};
+    var B = {x: box.x + box.width, y: box.y};
+    var C = {x: box.x, y: box.y + box.height};
+    return isBetween(A.x, point.x, B.x) && isBetween(A.y, point.y, C.y);
+  };
 
-var boxCollide = function(box1, box2){
-  return(
-    corners(box1).some(function(corner){return pointIsIn(corner, box2);})
-    || corners(box2).some(function(corner){return pointIsIn(corner, box1);}));
-};
+  var corners = function(box){
+    return(
+      [
+      {x: box.x, y: box.y},
+      {x: box.x + box.width, y: box.y},
+      {x: box.x + box.width, y: box.y + box.height},
+      {x: box.x, y: box.y + box.height}]
+      );
+  };
 
+  var boxCollide = function(box1, box2){
+    return(
+      corners(box1).some(function(corner){return pointIsIn(corner, box2);})
+      || corners(box2).some(function(corner){return pointIsIn(corner, box1);}));
+  };
 
 // TIMER
-  var startTimer = 0;
+  var startTimer = 0
   var timer = setInterval(function() {
       startTimer += 1;
       $('#counter').html(startTimer);
@@ -39,35 +42,18 @@ var boxCollide = function(box1, box2){
   };
 // New Game
   var newGame = function() {
-    window.location.reload();
+    document.location.reload(true);
   };
 
-(function() {
-
-$(document).ready(function() {
   // Theme music for gameplay duration
   // var simpsonsTheme = new Audio('audio/Simpsons_Theme.mp3');
   // simpsonsTheme.play();
 
-
-// TRYING LOCAL STORAGE
-//   var updateScore = $('.tops').html();
-//    localStorage.setItem('updateScore', updateScore);
-//    // return false;
-
-//    if(localStorage.getItem('updateScore')) {
-// $('.tops').html(localStorage.getItem('updateScore'));
-// }
-
 // Page loaded with landing page of just the Title and Play Button
   var $toggleView = function() {
     $('#game-area').hide();
-    $('#leaderboard').hide();
-    $('#inputData').hide();
-    $('#inputSubmit').hide();
     $('#timer').hide();
     $('.bart').hide();
-    $('#try-again').hide();
     $('#replay-btn').hide();
   }
   $toggleView();
@@ -75,64 +61,16 @@ $(document).ready(function() {
 // Start Game
   $('#play-btn').click(function() {
     $('#game-area').show();
-    $('#leaderboard').show();
     $('#objective').hide();
     $('#play-btn').hide();
     $('#timer').show();
     $('.bart').show();
     timer();
   })
-
+// Restart Game
   $('#replay-btn').click(function () {
     newGame();
   })
-
-
-// Create Nelson Muntz
-  // var createNelson = function() {
-  //   var $gameArea = $('#game-area');
-  //   var nelson = $('<div class="nelson"></div>');
-  //   $($gameArea).append(nelson);
-  //   setInterval(function() {
-  //     nelson.css("top", Math.random() * $gameArea.height());
-  //     nelson.css("left", Math.random() * $gameArea.width());
-  //   }, 2000)
-  //   // return nelson;
-  // }
-
-  // for(var i=0; i<5; i++) {
-  //   createNelson();
-  // }
-
-// Create Martin Prince
-  // var createMartin = function() {
-  //   var $gameArea = $('#game-area');
-  //   var martin = $('<div class="martin"></div>');
-  //   $('#game-area').append(martin);
-  //   setInterval(function() {
-  //     martin.css("top", Math.random() * $gameArea.height());
-  //     martin.css("left", Math.random() * $gameArea.width());
-  //   }, 2000)
-  // }
-  // for(var i=0; i<5; i++) {
-  //   createMartin();
-  // }
-
-// Create Binky
-  // var createBinky = function() {
-  //   var binky = $('<div class="binky"></div>');
-  //   $('#game-area').append(binky);
-  // }
-
-  // createBinky();
-
-// Create Sideshow Bob
-  // var createSideshowBob = function() {
-  //   var sideshowBob = $('<div class="sideshow-bob"</div>');
-  //   $('#game-area').append(sideshowBob);
-  // }
-
-  // createSideshowBob();
 
  var createSkinner = function() {
     var $gameArea = $('#game-area');
@@ -142,15 +80,16 @@ $(document).ready(function() {
       skinner.css("top",  Math.random() * $gameArea.height());
       skinner.css("left", Math.random() * $gameArea.width());
     }, 2000);
-    // skinner.attr('data', skinterval)
   }
-  for(var i=0; i<5; i++) {
+  for(var i=0; i<20; i++) {
     createSkinner();
   }
+
   var $bart = $('.bart');
   var $skinner = $('.skinner');
 
-function getCollision(skinner){
+//I worked alongside Leslie to try and get a basic understanding of collision detection.
+  function getCollision(skinner){
 
     $(skinner).each(function(index,skinner){
 
@@ -159,62 +98,28 @@ function getCollision(skinner){
       var $bartX = $bart.offset().left;
       var $bartY = $bart.offset().top;
 
-      // var $skinnerH = parseInt($(skinner).css('height').replace('px',''))
-      // var $skinnerW = parseInt($(skinner).css('width').replace('px',''))
-      // var $skinnerX = parseInt($(skinner).css('left').replace('px',''))
-      // var $skinnerY = parseInt($(skinner).css('top').replace('px',''))
-
       var $ourSkinner = $(skinner);
       var skinnerH = $ourSkinner.outerHeight(true);
       var skinnerW = $ourSkinner.outerWidth(true);
       var skinnerX = $ourSkinner.offset().left;
       var skinnerY = $ourSkinner.offset().top;
 
-      // console.log($skinnerX.left,'skinner');
-
       var bartBox = {x: $bartX, y: $bartY, width: $bartW, height: $bartH};
       var skinnerBox = {x: skinnerX, y: skinnerY, width: skinnerW, height: skinnerH};
 
-      if(boxCollide(bartBox, skinnerBox)){
-        console.log("we did it");
+      var score = parseInt($('#counter').html());
+
+      if (boxCollide(bartBox, skinnerBox)) {
+        alert('Congratulations! Your got a score of ' + score);
         $('#game-area').hide();
+        $('#timer').hide();
         $('.bart').hide();
-        $('#try-again').show();
         $('#replay-btn').show();
-        var score = $('#counter').html();
-        console.log(score);
         stopTimer();
-
-        var id = $('.id');
-        var top = $('.top-ten');
-        var spanner = $('<span class ="id"></span>')
-        var number = parseInt(id);
-
-        if (boxCollide) {
-          var userInput = prompt('Please enter your name for the leaderboard:');
-          for(var i=0; i<id.length; i++) {
-            if (score > i) {
-              $(top).html(userInput + " - ");
-              $(top).append(spanner);
-              $(spanner).html(score);
-            }
-          }
-          // for(var j=0; j<id.length; j++) {
-          //   if (score > j ) {
-          //     $(id).html(score);
-          //   }
-          // }
-
-
-          prompt.preventDefault();
-        } else {
-          alert('Sorry you didn\'t make it to the leaderboard.');
-          alert.preventDefault();
-        }
-
+        alert.preventDefault();
       }
     })
-}
+  }
 
   $("#game-area").mousemove(function(event) {
       $('.bart').css({
